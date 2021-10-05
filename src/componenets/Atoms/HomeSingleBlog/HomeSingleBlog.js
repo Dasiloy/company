4c1dc6bf6092552e74dbc0f61487b16d1cd7f966
oyewole damilare <oyewoledamilare00@gmail.com>
2021-10-05 02:50:29 +0100

    updated components

--- src/componenets/Atoms/HomeSingleBlog/HomeSingleBlog.js
+++ src/componenets/Atoms/HomeSingleBlog/HomeSingleBlog.js
@@ -1,11 +1,8 @@
 import React from "react";
 import "./HomeSingleBlog.css";
-import PropTypes from "prop-types";
 
 const HomeSingleBlog = (props) => {
   return <article></article>;
 };
-
-HomeSingleBlog.propTypes = {};
 
 export default HomeSingleBlog;
